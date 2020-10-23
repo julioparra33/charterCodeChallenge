5957c8e99affc37f829b2ecc9d5d9429d16acabb
Julio Parra Sanchez <parrajulio33@gmail.com>
2020-10-22 20:36:41 -0600

    Updates

---
+++ precache-manifest.24791f2f9530e67fa15549f3d998a263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfed37012cd9e7f8951040693d85e3a0",
+    "url": "/charterCodeChallenge/index.html"
+  },
+  {
+    "revision": "ed56db16be3060c6a947",
+    "url": "/charterCodeChallenge/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c2750d228367f4ce3dbc",
+    "url": "/charterCodeChallenge/static/css/main.4e55496a.chunk.css"
+  },
+  {
+    "revision": "ed56db16be3060c6a947",
+    "url": "/charterCodeChallenge/static/js/2.27ccb50c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/charterCodeChallenge/static/js/2.27ccb50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2750d228367f4ce3dbc",
+    "url": "/charterCodeChallenge/static/js/main.39c50027.chunk.js"
+  },
+  {
+    "revision": "877434ab3de852523697",
+    "url": "/charterCodeChallenge/static/js/runtime-main.70d015a6.js"
+  }
+]);
